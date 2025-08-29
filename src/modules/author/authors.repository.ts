bd3fa327fa83db856869f48ecfe5ce622939a3d9
juaniechaide <juaniechaide@gmail.com>
2025-08-29 01:01:10 -0300

    repository design patter change to typeorm injectrepository feat

--- src/modules/author/authors.repository.ts
+++ src/modules/author/authors.repository.ts
@@ -1,23 +1,39 @@
-import { DataSource, Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { Author } from './entities/author.entity';
 
 @Injectable()
-export class AuthorsRepository extends Repository<Author> {
-    constructor(private dataSource: DataSource) {
-        super(Author, dataSource.createEntityManager());
-    }
+export class AuthorsRepository {
+    constructor(
+        @InjectRepository(Author)
+        private readonly authorRepo: Repository<Author>,
+    ) { }
 
     async findWithBooks(): Promise<Author[]> {
-        return this.createQueryBuilder('author')
-            .leftJoinAndSelect('author.books', 'book')
-            .getMany();
+        return this.authorRepo.find({
+            relations: ['books'],
+        });
     }
 
     async findOneWithRelations(id: number): Promise<Author | null> {
-        return this.createQueryBuilder('author')
-            .leftJoinAndSelect('author.books', 'book')
-            .where('author.id = :id', { id })
-            .getOne();
+        return this.authorRepo.findOne({
+            where: { id },
+            relations: ['books'],
+        });
+    }
+
+    async createAuthor(data: Partial<Author>): Promise<Author> {
+        const author = this.authorRepo.create(data);
+        return this.authorRepo.save(author);
+    }
+
+    async updateAuthor(id: number, data: Partial<Author>): Promise<Author | null> {
+        await this.authorRepo.update(id, data);
+        return this.findOneWithRelations(id);
+    }
+
+    async removeAuthor(id: number): Promise<void> {
+        await this.authorRepo.delete(id);
     }
 }
--- src/modules/books/books.repository.ts
+++ src/modules/books/books.repository.ts
@@ -1,23 +1,39 @@
-import { DataSource, Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { Book } from './entities/book.entity';
 
 @Injectable()
-export class BooksRepository extends Repository<Book> {
-    constructor(private dataSource: DataSource) {
-        super(Book, dataSource.createEntityManager());
-    }
+export class BooksRepository {
+    constructor(
+        @InjectRepository(Book)
+        private readonly bookRepo: Repository<Book>,
+    ) { }
 
     async findWithAuthors(): Promise<Book[]> {
-        return this.createQueryBuilder('book')
-            .leftJoinAndSelect('book.authors', 'author')
-            .getMany();
+        return this.bookRepo.find({
+            relations: ['authors'],
+        });
     }
 
     async findOneWithRelations(id: number): Promise<Book | null> {
-        return this.createQueryBuilder('book')
-            .leftJoinAndSelect('book.authors', 'author')
-            .where('book.id = :id', { id })
-            .getOne();
+        return this.bookRepo.findOne({
+            where: { id },
+            relations: ['authors'],
+        });
+    }
+
+    async createBook(data: Partial<Book>): Promise<Book> {
+        const book = this.bookRepo.create(data);
+        return this.bookRepo.save(book);
+    }
+
+    async updateBook(id: number, data: Partial<Book>): Promise<Book | null> {
+        await this.bookRepo.update(id, data);
+        return this.findOneWithRelations(id);
+    }
+
+    async removeBook(id: number): Promise<void> {
+        await this.bookRepo.delete(id);
     }
 }
